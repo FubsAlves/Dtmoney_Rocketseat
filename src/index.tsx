6dc9c914acf4ff19de8db49dce59128e27368432
Fabiano Alves <fabianoxtx@gmail.com>
2022-05-20 15:17:21 -0300

    Projeto v1

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs';
+import { App }  from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+        transactions: [
+          {
+              id: 1,
+              title: 'Freelance de Website',
+              type: 'deposit',
+              category: 'Dev',
+              amount: 6000,
+              createdAt: new Date(Date.now())
+          },
+          {
+            id: 2,
+            title: 'Freelance de Website',
+            type: 'deposit',
+            category: 'Dev',
+            amount: 10000,
+            createdAt: new Date(Date.now())
+          },
+          {
+            id: 3,
+            title: 'Aluguel',
+            type: 'withdraw',
+            category: 'Despesa',
+            amount: -900,
+            createdAt: new Date(Date.now())
+          },
+          {
+            id: 4,
+            title: 'Mercado',
+            type: 'withdraw',
+            category: 'Despesa',
+            amount: -2000,
+            createdAt: new Date(Date.now())
+          },
+          {
+            id: 5,
+            title: 'Reforma',
+            type: 'withdraw',
+            category: 'Despesa',
+            amount: -2000,
+            createdAt: new Date(Date.now())
+          },
+          {
+            id: 6,
+            title: 'Poupança',
+            type: 'deposit',
+            category: 'Finanças',
+            amount: 1500,
+            createdAt: new Date(Date.now())
+          },
+        ],
+    })
+  },
+  
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+        const data = JSON.parse(request.requestBody);
+
+        return schema.create('transaction', data)
+    })
+
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +86,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
